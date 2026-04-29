f6b1a903c1e0f805aa3999c279a200d9e3c347af
Ekaterina <esamoilova@sfedu.ru>
2026-04-29 09:13:58 +0300

    Добавлена функция farewell

---
+++ script.js
@@ -0,0 +1,8 @@
+function greet(Ekaterina) {
+    return `Привет, ${Ekaterina}!`;
+    }
+    function farewell(Ekaterina) {
+    return `Пока, ${Ekaterina}!`;
+    }
+    console.log(greet('Мир'));
+    console.log(farewell('Друг'));
